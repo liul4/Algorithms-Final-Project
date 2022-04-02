d4fec12d20124a5b4bb883c54d81ce8acc8d8327
Liwen Liu <liul4@tcd.ie>
2022-04-02 15:58:21 +0100

    update BusSystem.java

--- BusManagementSystem/BusSystem.java
+++ BusManagementSystem/BusSystem.java
@@ -1,9 +1,32 @@
+import java.util.*;
+import java.io.*;
+
 
 public class BusSystem {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			FileReader file1 = new FileReader("stop_times");
+			Scanner stoptime = new Scanner(file1);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		try {
+			FileReader file2 = new FileReader("stops");
+			Scanner stop = new Scanner(file2);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		try {
+			FileReader file3 = new FileReader("transfers");
+			Scanner transfer = new Scanner(file3);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+        
 	}
 
 }
